9a2cb560dd9c3e77d03dd536487c2e9616daac87
JediGalaxy <123921849+JediGalaxy@users.noreply.github.com>
2023-03-04 18:31:27 +0700

    Update main.py
    Release a generation function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import random
 from tkinter.messagebox import showinfo
 
 clicks = 0
@@ -44,47 +45,75 @@ def selected(event):
 # -----------------------------------
 # ------Функция генерирования букв---
 def generate():
+    all_simbols = []
     if combobox.get() == "Английский":
         English_letters = ''                    # маленькие буквы
         for i in range(ord('a'), ord('z')+1):
             English_letters += chr(i)
-        print(English_letters)
+        all_simbols+=English_letters
+        #print(all_simbols)
 
         if checkbutton_enabledCl() == True:
             English_letters2 = ''
             for j in range(ord('A'), ord('Z')+1):   # заглавные буквы
                 English_letters2 += chr(j)
-            print(English_letters2)
+            all_simbols+=English_letters2
+        #print(all_simbols)
     elif combobox.get() == "Русский":
         Russian_letters = ''
         for k in range(ord('а'), ord('я')+1):   # маленькие буквы
             Russian_letters += chr(k)
-        print(Russian_letters)
+        all_simbols+=Russian_letters
+        #print(Russian_letters)
 
         if checkbutton_enabledCl() == True:
             Russian_letters2 = ''
             for k in range(ord('А'), ord('Я')+1):   # заглавные буквы
                 Russian_letters2 += chr(k)
-            print(Russian_letters2)
+            all_simbols+=Russian_letters2
+            #print(Russian_letters2)
+    elif combobox.get() == "0":
+        print("Выберите язык")
 
     if checkbutton_enabledS() == True:
         Simbols = ''
         for s in range(ord('!'), ord('/')+1):   # символы
             Simbols += chr(s)
-        print(Simbols)
+        all_simbols+=Simbols
+        #print(Simbols)
 
     if checkbutton_enabledN() == True:
         Numbers = ''
         for n in range(ord('0'), ord('9')+1):   # цифры
             Numbers += chr(n)
-        print(Numbers)
+        all_simbols+=Numbers
+        #print(Numbers)
 
     length_pass = spinbox.get()                 # берем длину пароля
 
-    if addWord == None:
-        return
+    if entry.get() == "Введите слово":
+        word = ''
+    elif entry.get() == '':
+        word = ''
     else:
         word = addWord                          # введенное слово
+        length_pass = int(length_pass)-len(word)
+
+    if len(all_simbols) >= int(length_pass):
+        pre_password = random.sample(all_simbols, int(length_pass))
+        if entry.get() != "Введите слово":
+            agregate_pass = list(word) + pre_password
+            #print(agregate_pass)
+            entry_pass.delete(0, END)
+            entry_pass.insert(0, ''.join(agregate_pass))
+        else:
+            #print(pre_password)
+            entry_pass.delete(0, END)
+            entry_pass.insert(0, ''.join(pre_password))
+    else:
+        return
+
+
 # -----------------------------------
 # ------Очистка полей----------------
 def clear():
@@ -101,8 +130,8 @@ root.geometry("400x400")
 
 # ------Кнопка Сгенерировать---------
 btn = ttk.Button(text="Сгенерировать пароль", command=generate)
-# btn.place(x=200, y=300, width = 150, height = 40)
 btn.pack(side=RIGHT, anchor=S, padx=34, pady=77)
+
 #------------------------------------
 # ------Кнопка показать пароль-------
 btn_show = ttk.Button(text="Показать пароль", command=show_password)
@@ -113,19 +142,19 @@ enter_button.place(x=230, y=136)
 # -----------------------------------
 
 # ------Кнопка Очистить--------------
-clear_button = ttk.Button(text="Clear", command=clear)
+clear_button = ttk.Button(text="Очистить", command=clear)
 clear_button.place(x=310, y=136)
 # -----------------------------------
 # ------Лого в контейнере------------
-BounsLogo = PhotoImage(file="roflan.gif")
+BounsLogo = PhotoImage(file="billy.png")
 labelLogo = ttk.Label(image=BounsLogo)
-labelLogo.place(x=6, y=6)
+labelLogo.place(x=50, y=6)
 # -----------------------------------
 
 # ------Поле ввода-------------------
 entry = ttk.Entry()
 entry.place(x=230, y=110)
-entry.insert(0, "Введите текст")
+entry.insert(0, "Введите слово")
 # ------Поле получения пароля--------
 entry_pass = ttk.Entry()
 entry_pass.place(x=20, y=300)
